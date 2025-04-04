d1b97ed40a0f1d2f78e9f611a5638306d54e4f72
Yoon6 <yoonda5898@gmail.com>
2025-04-04 23:51:17 +0900

    emscripten 모듈 사용하도록 변경.
    wasi 관련 오류 해결 못함.

--- main.cpp
+++ main.cpp
@@ -12,40 +12,51 @@ Program* parse(vector<Token> tokenList);
 tuple<vector<Code>, map<wstring, size_t>> generate(Program* syntaxTree);
 void execute(vector<Code> codeList, map<wstring, size_t> functionTables);
 
-// auto printTokenList(vector<Token>) -> void;
+auto printTokenList(vector<Token>) -> void;
 // auto printSyntaxTree(Program*) -> void;
 // auto printObjectCode(tuple<vector<Code>, map<wstring, size_t>>) -> void;
+extern "C" void run(string sourceCode)
+{
+    printf("Run\n");
+    printf("Scan\n");
+    vector<Token> tokenList = scan(sourceCode);
+    printTokenList(tokenList);
+    printf("Parse\n");
+	Program* syntaxTree = parse(tokenList);
+    printf("Generate\n");
+	auto objectCode = generate(syntaxTree);
+    printf("Execute\n");
+    execute(get<0>(objectCode), get<1>(objectCode));
+}
+
 
 int main() {
-    string sourceCode = R"(
-        함수 시작() {
-            출력줄바꿈(11 + 2);
-            출력줄바꿈("안녕하세요");
-            출력줄바꿈(팩토리얼(3));
-        }
+//     string sourceCode = R"(
+//         함수 시작() {
+//             출력줄바꿈(11 + 2);
+//             출력줄바꿈("Hello");
+//             출력줄바꿈(팩토리얼(3));
+//         }
 
-        함수 팩토리얼(숫자) {
-            만약 (숫자 < 2) {
-                반환 1;
-            }
-            반환 숫자 * 팩토리얼(숫자 - 1);
-        }
-    )";
+//         함수 팩토리얼(숫자) {
+//             만약 (숫자 < 2) {
+//                 반환 1;
+//             }
+//             반환 숫자 * 팩토리얼(숫자 - 1);
+//         }
+//     )";
 
-    vector<Token> tokenList = scan(sourceCode);
-	Program* syntaxTree = parse(tokenList);
-	auto objectCode = generate(syntaxTree);
-    execute(get<0>(objectCode), get<1>(objectCode));
+//     run(sourceCode);
 
     return 0;
 }
 
-// auto printTokenList(vector<Token> tokenList) -> void {
-//     cout << setw(12) << left << "KIND" << "STRING" << endl;
-//     cout << string(23, '-') << endl;
-//     for (auto &token: tokenList)
-//         cout << token << endl;
-// }
+auto printTokenList(vector<Token> tokenList) -> void {
+    cout << setw(12) << left << "KIND" << "STRING" << endl;
+    cout << string(23, '-') << endl;
+    for (auto &token: tokenList)
+        cout << token << endl;
+}
 
 auto operator<<(ostream &stream, Token &token) -> ostream & {
     std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
